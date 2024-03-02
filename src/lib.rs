1042c1da107333d42ca483151d1538b7cc2991cf
Gunnar Omander <gunnar@gomander.dev>
2024-03-02 14:27:13 +0100

    Remove custom allocators

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,6 @@ use napi_derive::module_exports;
 
 mod encoder;
 
-#[cfg(all(unix, not(target_env = "musl"), not(target_arch = "aarch64")))]
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-#[cfg(all(windows, target_arch = "x86_64"))]
-#[global_allocator]
-static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
-
 #[module_exports]
 fn init(mut exports: JsObject, env: Env) -> Result<()> {
     let encoder = encoder::create_js_class(&env)?;
